a0e7747ab0222b4361274b3b94dd0d3cfe8388ae
iamnotrodger <rodger.retanal24@gmail.com>
2021-05-07 01:09:16 -0400

    [feature] Added link to Github page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import MovieList from './component/MovieList/MovieList';
 import NominationBanner from './component/NominationBanner';
 import NominationList from './component/NominationList';
 import Search from './component/Search';
+import GithubIcon from './icon/GithubIcon';
 
 import './App.css';
 
 const App = () => {
     return (
         <div className='app'>
-            <header className='app__header'>
-                <h1 className='app__title'>The Shoppies</h1>
+            <header className='header'>
+                <div className='app__header'>
+                    <h1 className='app__title'>The Shoppies</h1>
+                    <a
+                        href='https://github.com/iamnotrodger/shoppies'
+                        className='app__github'>
+                        <GithubIcon /> GitHub
+                    </a>
+                </div>
                 <Search />
             </header>
             <main className='main'>
